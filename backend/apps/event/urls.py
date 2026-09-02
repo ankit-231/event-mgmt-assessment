42fcb7b2943fca7524dd161be97609198b244a14
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 15:44:43 +0545

    feat: retrieve, update, delete, event endpoints

--- backend/apps/event/urls.py
+++ backend/apps/event/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.ListCreateEventView.as_view(), name="list_create_event"),
-    # path("<int:event_id>/", views.ListCreateEventView.as_view(), name="list_create_event"),
+    path(
+        "<int:event_id>/",
+        views.RetrieveUpdateDeleteEventView.as_view(),
+        name="retrieve_update_delete_event",
+    ),
 ]
--- backend/apps/event/views.py
+++ backend/apps/event/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 from apps.core.utils.base_views import BaseAPIView
-from apps.core.utils.response_wrappers import OKResponse
+from apps.core.utils.response_wrappers import NoContentResponse, OKResponse
 from apps.event.models import Event
-from apps.event.serializers import CreateEventSerializer, GetEventDetailSerializer
+from apps.event.serializers import (
+    CreateEventSerializer,
+    GetEventDetailSerializer,
+    UpdateEventSerializer,
+)
 from apps.event.utils import EventService
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -27,3 +32,30 @@ class ListCreateEventView(BaseAPIView):
         output_data = self.output_serializer(event).data
 
         return OKResponse(data=output_data)
+
+
+class RetrieveUpdateDeleteEventView(BaseAPIView):
+    output_serializer = GetEventDetailSerializer
+
+    input_serializer = UpdateEventSerializer
+
+    def get(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+
+        data = self.output_serializer(event).data
+        return OKResponse(data=data)
+
+    def patch(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+
+        data = request.data
+        event = EventService().update(event=event, data=data)
+
+        output_data = self.output_serializer(event).data
+
+        return OKResponse(data=output_data)
+
+    def delete(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+        event.delete()
+        return NoContentResponse()
