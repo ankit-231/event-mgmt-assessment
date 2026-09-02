c7505d87ae4a6c7819de22b161b855410c9a06bf
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 16:53:27 +0545

    feat: create api endpoints

---
+++ frontend/lib/api-endpoints.ts
@@ -0,0 +1,17 @@
+import { EventId } from "@/types/events"
+
+export const apiEndpoints = {
+  events: {
+    list: "/events/",
+    create: "/events/",
+    detail: (eventId: EventId) => `/events/${eventId}/`,
+    update: (eventId: EventId) => `/events/${eventId}/`,
+    delete: (eventId: EventId) => `/events/${eventId}/`,
+    analytics: "/events/analytics/",
+  },
+} as const
+
+export type ApiEndpoints = typeof apiEndpoints
+
+export const APIBaseURL =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api/v1"
