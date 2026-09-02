e03640298964eda2aba9bfd4a51a100030e14001
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 15:42:46 +0545

    feat: create EventService.update

--- backend/apps/event/utils.py
+++ backend/apps/event/utils.py
@@ -1,4 +1,5 @@
-from apps.event.serializers import CreateEventSerializer
+from apps.core.utils.common import update_model_instance
+from apps.event.serializers import CreateEventSerializer, UpdateEventSerializer
 
 from .models import Event
 
@@ -16,6 +17,20 @@ class EventService:
     def _validate_create_data(self, data: dict) -> dict:
         serializer = CreateEventSerializer(data=data)
         # handled by exception handler
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        return validated_data
+
+    def update(self, event: Event, data: dict) -> Event:
+        validated_data = self._validate_update_data(data)
+        # set validate_fields_exist because we rely on ModelSerializer to validate fields
+        update_model_instance(
+            instance=event, validate_fields_exist=False, **validated_data
+        )
+        return event
+
+    def _validate_update_data(self, data: dict) -> dict:
+        serializer = UpdateEventSerializer(data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
         return validated_data
