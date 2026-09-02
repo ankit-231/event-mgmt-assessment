43927c7e102d41f4f59169d43b6103a1a00fbf71
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 16:27:02 +0545

    feat: create EventAnalyticService.get_last_24_hours_metrics

--- backend/apps/event/utils.py
+++ backend/apps/event/utils.py
@@ -1,3 +1,6 @@
+from django.db.models import Count
+from django.utils import timezone
+
 from apps.core.utils.common import update_model_instance
 from apps.event.serializers import CreateEventSerializer, UpdateEventSerializer
 
@@ -34,3 +37,21 @@ class EventService:
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
         return validated_data
+
+
+class EventAnalyticService:
+
+    def get_last_24_hours_metrics(self) -> dict:
+        since = timezone.now() - timezone.timedelta(hours=24)
+
+        events = Event.objects.filter(start_time__gte=since)
+
+        counts_by_type = {
+            row["event_type"]: row["count"]
+            for row in events.values("event_type").annotate(count=Count("id"))
+        }
+
+        return {
+            "total": events.count(),
+            "counts_by_type": counts_by_type,
+        }
