9f1958da46bba7cb21c864a65599062ee3153428
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 15:15:39 +0545

    feat: create event model and migration file

--- backend/apps/event/models.py
+++ backend/apps/event/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from apps.core.utils.base_models import BaseModel
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Event(BaseModel):
+    class EventType(models.TextChoices):
+        CONFERENCE = "Conference", "Conference"
+        MEETUP = "Meetup", "Meetup"
+        WORKSHOP = "Workshop", "Workshop"
+        WEBINAR = "Webinar", "Webinar"
+        OTHER = "Other", "Other"
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    payload = models.JSONField()
+
+    class Meta:
+        db_table = "event"
+        verbose_name = "Event"
+        verbose_name_plural = "Events"
