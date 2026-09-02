f6bc3a155aa063c16dd32b7a60951ae711ca3129
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 16:51:21 +0545

    feat: create query keys for tanstack query

---
+++ frontend/lib/query-keys.ts
@@ -0,0 +1,16 @@
+import { EventId, EventListFilters } from "@/types/events"
+
+export const eventKeys = {
+  all: () => ["events"] as const,
+
+  lists: () => [...eventKeys.all(), "list"] as const,
+  list: (filters: EventListFilters) =>
+    [...eventKeys.lists(), filters] as const,
+  listInfinite: (filters: Omit<EventListFilters, "page">) =>
+    [...eventKeys.lists(), "infinite", filters] as const,
+
+  details: () => [...eventKeys.all(), "detail"] as const,
+  detail: (id: EventId) => [...eventKeys.details(), id] as const,
+
+  analytics: () => [...eventKeys.all(), "analytics"] as const,
+}
