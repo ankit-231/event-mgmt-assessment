53c716b57ec8fe89458380e5b7d9deb2a2e5c5cd
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-02 15:22:12 +0545

    feat: create minimal views for list and create event

--- backend/apps/event/views.py
+++ backend/apps/event/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from apps.core.utils.base_views import BaseAPIView
+from apps.core.utils.response_wrappers import OKResponse
+from apps.event.models import Event
+from apps.event.serializers import CreateEventSerializer, GetEventDetailSerializer
+
 # Create your views here.
+
+
+class ListCreateEventView(BaseAPIView):
+    output_serializer = GetEventDetailSerializer
+
+    input_serializer = CreateEventSerializer
+
+    def get(self, request):
+        events = Event.objects.all()
+
+        data = self.output_serializer(events, many=True).data
+        return OKResponse(data=data)
+
+    def post(self, request):
+        serializer = self.output_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        event = serializer.save()
+        return OKResponse(data=self.output_serializer(event).data)
